c6ff8f963884defe610b6946252d335f1ec43fc9
JoseManuelQuesada <JoseManuelQuesada@DESKTOP-ITS2582>
2022-03-29 13:07:32 +0200

    Segunda subida

---
+++ src/otra_clase.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author JoseManuelQuesada
+ *
+ */
+public class otra_clase {
+
+	/**
+	 * 
+	 */
+	public otra_clase() {
+		// TODO Auto-generated constructor stub
+		System.out.print("Hola");
+	}
+
+}
